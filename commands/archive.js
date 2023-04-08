f528d5d70cabce4da733a8afa0359016a411a238
Fire <annabeth.phoenix@gmail.com>
2023-04-08 16:36:49 -0500

    added archive file

--- commands/archive.js
+++ commands/archive.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { channelId } = require('../config.json');
+const fs = require('fs').promises;
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
@@ -8,15 +9,35 @@ module.exports = {
         .addStringOption(option =>
             option.setName('message_id')
                 .setDescription('The message to archive.')
-                .setRequired(true)),
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('date')
+                .setDescription('The date of the DMP in "YYYY-MM-DD" format. Set to the date of submission if not included.')),
     async execute(interaction) {
         const channel = interaction.client.channels.cache.get(channelId);
         const msgId = interaction.options.get("message_id").value;
 
         channel.messages.fetch(msgId)
-        .then(message => {
-            const msg = message.content;
-            console.log(msg);
+        .then(async message => {
+            let date;
+            if (interaction.options.get("date")) date = interaction.options.get("date").value;
+            else date = formatTimestamp(message.createdTimestamp);
+
+            let archive = require('../archives.json');
+            archive[date] = message;
+
+            await fs.writeFile('./archives.json', JSON.stringify(archive));
+
+            await interaction.reply("Successfully archived.");
         }).catch(console.error);
     },
 };
+
+function formatTimestamp(timestamp) {
+    let date = new Date(timestamp);
+    let day = date.getDate().toLocaleString(undefined, {minimumIntegerDigits: 2});
+    let month = (date.getMonth() + 1).toLocaleString(undefined, {minimumIntegerDigits: 2});
+    let year = date.getFullYear().toLocaleString(undefined, {minimumIntegerDigits: 4}).replace(",", "");
+
+    return year + "-" + month + "-" + day;
+}
