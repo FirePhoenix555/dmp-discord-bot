3db203fe2c3dcdb47ec70436389d4695dd5a6a3f
Fire <annabeth.phoenix@gmail.com>
2023-04-08 14:54:33 -0500

    fixed command name

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
 	data: new Discord.SlashCommandBuilder()
-		.setName('hello world')
+		.setName('helloworld')
 		.setDescription('sends hello world msg'),
 	async execute(interaction) {
 		await interaction.reply('Hello, world!');
