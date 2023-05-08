0f20919a33df5e140c2198502509f3fbcbf25eaa
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-05-08 17:07:48 -0500

    fixed command name in incomplete command warning

--- util/commands.js
+++ util/commands.js
@@ -4,7 +4,7 @@ module.exports = {
             if ('data' in cmd && 'execute' in cmd)
                 cb(cmd);
             else
-                console.log(`[WARNING] The command ${cmd} is missing a required "data" or "execute" property.`);
+                console.log(`[WARNING] The command ${(cmd.data && cmd.data.name) ? cmd.data.name : "[undefined]"} is missing a required "data" or "execute" property.`);
         });
     }
 }
